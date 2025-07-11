350ba07e1c71cca25aec81d094759f29b39cab47
timkoffy <nikitakirilltimur@gmail.com>
2025-07-11 23:07:14 +0300

    фикс криворуких

--- CatCoffee/Program.cs
+++ CatCoffee/Program.cs
@@ -4,23 +4,40 @@ public class Program
 {
     public static void Main()
     {
-        Cosmetics.AnimatingString(new[]
+        string selectedCat;
+        void Start()
         {
-            "Добро пожаловать в котокафе МурМяу",
-            "Какую кошку хотите погладить?\n",
-            "[1] Рыжая, [2] Бело-серая в полосочку, [3] Черненькая\n"
-        });
+            Cosmetics.AnimatingString(new[]
+            {
+                "Добро пожаловать в котокафе МурМяу",
+                "Какую кошку хотите погладить?\n",
+                "[1] Рыжая, [2] Бело-серая в полосочку, [3] Черненькая\n"
+            });
+            selectedCat = Console.ReadLine();
+            Console.Clear();
+            
+            try
+            {
+                int selectedCatInt = int.Parse(selectedCat);
+
+                Cat[] cats = new Cat[4];
+                cats[1] = new SmartyCat("Смарти", "Мяу");
+                cats[2] = new LinyCat("Лини", "Мурр");
+                cats[3] = new BlackyCat("Блэки", "Мяу мяу мяу");
+
+                cats[selectedCatInt].Print();
+            }
+            catch
+            {
+                Cosmetics.AnimatingString(new[] {"Пожалуйста, введите числа 1, 2 или 3."});
+                Console.ReadKey();
+                Console.Clear();
+                Start();
+            }
+        }
+
+        Start();
         
-        string selectedCat = Console.ReadLine();
         
-        Console.Clear();
-        int selectedCatInt = int.Parse(selectedCat);
-        
-        Cat[] cats = new Cat[4];
-        cats[1] = new SmartyCat("Смарти", "Мяу");
-        cats[2] = new LinyCat("Лини", "Мурр");
-        cats[3] = new BlackyCat("Блэки", "Мяу мяу мяу");
-        
-        cats[selectedCatInt].Print();
     }
 }
